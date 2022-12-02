f656ce8c9195cd2e8cce5699eb474e53ef8a0e53
VolodymyrShostak <110408009+VolodymyrShostak@users.noreply.github.com>
2022-12-02 23:19:54 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Routes, Route }  from 'react-router-dom';
 import  HomePage  from '../pages/HomePage.jsx';
-import  MoviesPage  from '../pages/MoviesPage.jsx';
+import MoviesPage from '../pages/MoviesPage.jsx';
+import { Layout } from './Layout/Layout.jsx';
 
 
 export const App = () => {
   return (
-    <div>
+    <>
       <Routes>
+        <Route path="/" element={<Layout /> }></Route>
         <Route path="/home" element={<HomePage />} />
         <Route path="/movies" element={<MoviesPage />} />
       </Routes>
-    </div>
+    </>
   );
 };
 // 50f0044b98fb36babd948adb82a325bf
---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Layout = () => {
+    return (
+      <div>
+        <div>Header</div>
+        <div>Content</div>
+      </div>
+    );
+}
