2aaf303b2fa53822406f4c11ab3e549de0bb48d4
VolodymyrShostak <110408009+VolodymyrShostak@users.noreply.github.com>
2022-12-07 18:41:12 +0200

    fixed mistake

--- src/components/MovieDetailsCast/MovieDetailsCast.jsx
+++ src/components/MovieDetailsCast/MovieDetailsCast.jsx
@@ -39,7 +39,7 @@ function MovieDetailsCast() {
           return (
             <WrapperActor key={original_name}>
               <Image
-                src={profile_path ? apiURL + profile_path : 'https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM='
+                src={profile_path ? apiURL + profile_path : 'https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM='}
                 alt="actor"
               />
               ,<Text>{original_name}</Text>
