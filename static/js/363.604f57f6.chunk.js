4b3473cb907e1c18b95c42e0528120c9b286a690
VolodymyrShostak <110408009+VolodymyrShostak@users.noreply.github.com>
2022-12-07 12:22:37 +0000

    Deploying to gh-pages from @ VolodymyrShostak/goit-react-hw-05-movies@562a71d480acd196ad16a71dc9b57616ff522863 🚀

---
+++ static/js/363.604f57f6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[363],{363:function(e,n,t){t.r(n),t.d(n,{default:function(){return o}});var i,r=t(168),s=t(444).ZP.div(i||(i=(0,r.Z)(["\n  display:flex;\n  \n"]))),c=t(184),o=function(){return(0,c.jsx)(s,{as:"main",display:"flex",textAlign:"center",justifyContent:"center",padding:"20px",color:"white",children:(0,c.jsx)("h1",{children:"Sorry, this page not found..."})})}}}]);
+//# sourceMappingURL=363.604f57f6.chunk.js.map
